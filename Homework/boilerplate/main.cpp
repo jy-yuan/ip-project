75f61465c1c916d225818ce2f605f59e7ec939f7
袁加熠 <2017011411@secoder.net>
2019-12-11 00:17:32 +0800

    12.10

--- Homework/boilerplate/main.cpp
+++ Homework/boilerplate/main.cpp
@@ -15,12 +15,12 @@ extern uint32_t assemble(const RipPacket *rip, uint8_t *buffer);
 
 uint8_t packet[2048];
 uint8_t output[2048];
-// 0: 10.0.0.1
-// 1: 10.0.1.1
+// 0: 192.168.3.2
+// 1: 192.168.4.1
 // 2: 10.0.2.1
 // 3: 10.0.3.1
 // 你可以按需进行修改，注意端序
-in_addr_t addrs[N_IFACE_ON_BOARD] = {0x0100000a, 0x0101000a, 0x0102000a,
+in_addr_t addrs[N_IFACE_ON_BOARD] = {0x0203a8c0, 0x0104a8c0, 0x0102000a,
                                      0x0103000a};
 
 int main(int argc, char *argv[]) {
@@ -54,6 +54,61 @@ int main(int argc, char *argv[]) {
       // send complete routing table to every interface
       // ref. RFC2453 3.8
       // multicast MAC for 224.0.0.9 is 01:00:5e:00:00:09
+      output[0] = 0x45;
+      output[1] = 0x00;
+      output[2] = 0x00;
+      output[3] = 0x34;//length=52
+      output[4] = 0x00;
+      output[5] = 0x00;
+      output[6] = 0x00;
+      output[7] = 0x00;
+      output[8] = 0x01;
+      output[9] = 0x11;
+      output[12] = (uint8_t)(dst_addr & 0xff);
+      output[13] = (uint8_t)((dst_addr >> 8) & 0xff);
+      output[14] = (uint8_t)((dst_addr >> 16) & 0xff);
+      output[15] = (uint8_t)((dst_addr >> 24) & 0xff);
+      output[16] = 0x09;
+      output[17] = 0x00;
+      output[18] = 0x00;
+      output[19] = 0xe0;
+      // ...
+      // UDP
+      // port = 520
+      output[20] = 0x02;
+      output[21] = 0x08;
+      output[22] = 0x02;
+      output[23] = 0x08;
+      output[24] = 0x00;
+      output[25] = 0x20;
+      output[26] = 0x00;
+      output[27] = 0x00;
+      // ...
+      // RIP
+      output[28] = 1;
+      output[29] = 2;
+      // ...
+      for(int i = 30; i < 53; i++) {
+        output[i] = 0;
+      }
+      output[53] = 0x10;
+      unsigned long checksum = 0;
+      for (uint8_t i = 0; i < length; i += 2) {
+        if (i != 10) {
+          checksum += (((unsigned long)packet[i] << 8) + (unsigned long)packet[i + 1]);
+        }
+      }
+      checksum = (checksum >> 16) + (checksum & 0xffff);
+      checksum += checksum >> 16;
+      output[10] = (uint8_t)((~checksum) >> 8);
+      output[11] = (uint8_t)(~checksum);
+      uint32_t rip_len = 0x34;
+      macaddr_t dst_mac;
+      //TODO: get ifindex & dst_mac
+      in_add
+       = HAL_ArpGetMacAddress(,0x090000e0 , dst_mac);
+       //
+      HAL_SendIPPacket(if_index, output, rip_len, dst_mac);
       printf("30s Timer\n");
       last_time = time;
     }
@@ -82,6 +137,14 @@ int main(int argc, char *argv[]) {
       continue;
     }
     in_addr_t src_addr, dst_addr;
+    src_addr = (in_addr_t)packet[12];
+    src_addr += ((in_addr_t)packet[13]) << 8;
+    src_addr += ((in_addr_t)packet[14]) << 16;
+    src_addr += ((in_addr_t)packet[15]) << 24;
+    dst_addr = (in_addr_t)packet[16];
+    dst_addr += ((in_addr_t)packet[17]) << 8;
+    dst_addr += ((in_addr_t)packet[18]) << 16;
+    dst_addr += ((in_addr_t)packet[19]) << 24;
     // extract src_addr and dst_addr from packet
     // big endian
 
@@ -94,6 +157,9 @@ int main(int argc, char *argv[]) {
       }
     }
     // TODO: Handle rip multicast address(224.0.0.9)?
+    if (dst_addr == 0x090000e0) {
+      dst_is_me = true;
+    }
 
     if (dst_is_me) {
       // 3a.1
@@ -108,16 +174,52 @@ int main(int argc, char *argv[]) {
           // assemble
           // IP
           output[0] = 0x45;
+          output[1] = 0x00;
+          output[4] = 0x00;
+          output[5] = 0x00;
+          output[6] = 0x00;
+          output[7] = 0x00;
+          output[8] = 0x01;
+          output[9] = 0x11;
+          output[12] = (uint8_t)(dst_addr & 0xff);
+          output[13] = (uint8_t)((dst_addr >> 8) & 0xff);
+          output[14] = (uint8_t)((dst_addr >> 16) & 0xff);
+          output[15] = (uint8_t)((dst_addr >> 24) & 0xff);
+          output[16] = (uint8_t)(src_addr & 0xff);
+          output[17] = (uint8_t)((src_addr >> 8) & 0xff);
+          output[18] = (uint8_t)((src_addr >> 16) & 0xff);
+          output[19] = (uint8_t)((src_addr >> 24) & 0xff);
           // ...
           // UDP
           // port = 520
           output[20] = 0x02;
           output[21] = 0x08;
+          output[22] = 0x02;
+          output[23] = 0x08;
           // ...
           // RIP
           uint32_t rip_len = assemble(&resp, &output[20 + 8]);
+          output[2] = (uint8_t) ((rip_len + 28) >> 8);
+          output[3] = (uint8_t) rip_len + 28;
+          output[24] = (uint8_t) ((rip_len + 8) >> 8);
+          output[25] = (uint8_t) rip_len + 8;
+          // TODO: checksum
           // checksum calculation for ip and udp
+          unsigned long checksum = 0;
+          for (uint8_t i = 0; i < length; i += 2) {
+            if (i != 10) {
+              checksum += (((unsigned long)packet[i] << 8) + (unsigned long)packet[i + 1]);
+            }
+          }
+          checksum = (checksum >> 16) + (checksum & 0xffff);
+          checksum += checksum >> 16;
+          output[10] = (uint8_t)((~checksum) >> 8);
+          output[11] = (uint8_t)(~checksum);
+          output[26] = 0x00;
+          output[27] = 0x00;
           // if you don't want to calculate udp checksum, set it to zero
+          output[26] = 0x00;
+          output[27] = 0x00;
           // send it back
           HAL_SendIPPacket(if_index, output, rip_len + 20 + 8, src_mac);
         } else {
